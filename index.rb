497cbb23ea75c5a12328c5fe9c4f400c20b2b69e
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-02-16 13:53:33 -0500

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,28 +1,29 @@
- 1 #! /usr/bin/ruby
- 2
- 3 # Scramble words.
- 4
- 5 class String
- 6   def scramble
- 7     s = self.split(//).sort_by { rand }.join(”)
- 8     s =~ /[A-Z]/ && s =~ /[a-z]/ ? s.capitalize : s
- 9   end
-10
-11   def scramble_words
-12     ret = []
-13     self.split(/\s+/).each { |nws|
-14       nws.scan(/^(\W*)(\w*)(\W*)$/) { |pre, word, post|
-15         ret << pre + word.scramble + post
-16       }
-17     }
-18
-19     ret.join " "
-20   end
-21 end
-22
-23 loop do
-24   print "Enter something: "
-25   str = gets.chomp
-26   exit if str.empty?
-27   puts str.scramble_words
-28 end
+  #! /usr/bin/ruby
+ 
+  # Scramble words.
+ 
+  class String
+    def scramble
+      s = self.split(//).sort_by { rand }.join(”)
+      s =~ /[A-Z]/ && s =~ /[a-z]/ ? s.capitalize : s
+    end
+
+   def scramble_words
+     ret = []
+     self.split(/\s+/).each { |nws|
+       nws.scan(/^(\W*)(\w*)(\W*)$/) { |pre, word, post|
+         ret << pre + word.scramble + post
+       }
+     }
+
+     ret.join " "
+   end
+ end
+
+loop do
+  print "Enter something: "
+  $stdout.flush
+  str = gets.chomp
+  exit if str.empty?
+  puts str.scramble_words
+end
